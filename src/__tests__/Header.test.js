189874033188bd13d4bb99658ad31c95128e465f
Joyce Sheng <joycesheng96@hotmail.com>
2024-10-28 16:17:53 -0400

    saving lab

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,11 +1,14 @@
+// import libraries needed for testing
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import Header from "../components/Header";
+// import the component you wrote
+import Article from "../components/Article";
 
-test("displays the text 'hello from the Header!'", () => {
-  render(<Header />);
+// test the component
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
 
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
